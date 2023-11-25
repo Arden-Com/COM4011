530aa1e1685675211c111650cb36514ad15bc3ea
Derek Peacock <derek.peacock@yahoo.com>
2023-11-25 11:24:11 +0000

    Created heading

--- Examples/src/App.java
+++ Examples/src/App.java
@@ -1,5 +1,10 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception 
+    {
+        System.out.println();
+        System.out.println("============================");
+        System.out.println("Arden COM-4011 Java Examples");
+        System.out.println("============================");
+        System.out.println();
     }
 }
